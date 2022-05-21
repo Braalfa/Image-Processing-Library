508d335774b1c1446f5286baedf0d45d823e14f4
Brayan <alfabrayan12@gmail.com>
2022-05-21 15:00:29 -0600

    Morphological filters

--- Base/main.cpp
+++ Base/main.cpp
@@ -71,14 +71,14 @@ void printMat(Mat mat){
     }
 }
 
-void averageFilter(Mat image){
+void averageFilter(Mat image, int iterations){
 
     image.convertTo(image, CV_64F);
     vector<Mat> channels(3);
     split(image, channels);
     int k = 3;
     cv::Mat matB(k, k, CV_64F, 1/(pow(k,2)));
-    for(int i = 0; i< 20; i++){
+    for(int i = 0; i< iterations; i++){
         channels[0] = convolution2D(channels[0], matB);
         channels[1] = convolution2D(channels[1], matB);
         channels[2] = convolution2D(channels[2], matB);
@@ -88,10 +88,127 @@ void averageFilter(Mat image){
     imwrite("averageConvolutionalFilterOutput.jpg", final);
 }
 
+int findMaxInMatRange(Mat mat, int xmin, int xmax, int ymin, int ymax){
+    double max = -INFINITY;
+    for(int x=xmin; x<xmax;x++){
+        for(int y=ymin; y<ymax;y++){
+            if (mat.at<double>(x,y) > max){
+                max = mat.at<double>(x,y);
+            }
+        }
+    }
+    return max;
+}
+
+int findMinInMatRange(Mat mat, int xmin, int xmax, int ymin, int ymax){
+    double min = INFINITY;
+    for(int x=xmin; x<xmax;x++){
+        for(int y=ymin; y<ymax;y++){
+            if (mat.at<double>(x,y) < min){
+                min = mat.at<double>(x,y);
+            }
+        }
+    }
+    return min;
+}
+
+Mat dilatation(Mat* imagePtr){
+    Mat image = *imagePtr;
+    int m = image.rows;
+    int n = image.cols;
+    cv::Mat out(m, n, CV_64F, 0.0);
+
+    for(int x=0; x<m; x++){
+        int xmin = max(x-1,0);
+        int xmax = min(x+2,m);
+        for(int y=0; y<n; y++){
+            int ymin = max(y-1,0);
+            int ymax = min(y+2,n);
+            out.at<double>(x,y)= findMaxInMatRange(image, xmin, xmax, ymin, ymax);
+        }
+    }
+    return out;
+}
+
+Mat erotion(Mat* imagePtr){
+    Mat image = *imagePtr;
+    int m = image.rows;
+    int n = image.cols;
+    cv::Mat out(m, n, CV_64F, 0.0);
+
+    for(int x=0; x<m; x++){
+        int xmin = max(x-1,0);
+        int xmax = min(x+2,m);
+        for(int y=0; y<n; y++){
+            int ymin = max(y-1,0);
+            int ymax = min(y+2,n);
+            out.at<double>(x,y)= findMinInMatRange(image, xmin, xmax, ymin, ymax);
+        }
+    }
+    return out;
+}
+
+Mat open(Mat* imagePtr){
+    Mat tmp = erotion(imagePtr);
+    Mat out = dilatation(&tmp);
+    return out;
+}
+
+Mat clausure(Mat* imagePtr){
+    Mat tmp = dilatation(imagePtr);
+    Mat out = erotion(&tmp);
+    return out;
+}
+
+Mat topHat(Mat* imagePtr){
+    Mat image = *imagePtr;
+    Mat out = open(imagePtr);
+    for(int i = 0; i <image.rows; i++)
+    {
+        for(int j = 0; j < image.cols; j++)
+        {
+            out.at<double>(i, j) = image.at<double>(i, j) - out.at<double>(i, j);
+        }
+    }
+    return out;
+}
+
+Mat bottomHat(Mat* imagePtr){
+    Mat image = *imagePtr;
+    Mat out = clausure(imagePtr);
+    for(int i = 0; i < image.rows; i++)
+    {
+        for(int j = 0; j < image.cols; j++)
+        {
+            out.at<double>(i, j) = out.at<double>(i, j) -  image.at<double>(i, j);
+        }
+    }
+    return out;
+}
+
+void contrastEnhancementFilter(Mat* imagePtr){
+    Mat image = *imagePtr;
+    Mat topHatImage = topHat(imagePtr);
+    Mat bottomHatImage = bottomHat(imagePtr);
+    cv::Mat out(image.rows, image.cols, CV_64F, 0.0);
+    for(int i = 0; i < image.rows; i++)
+    {
+        for(int j = 0; j < image.cols; j++)
+        {
+            out.at<double>(i, j) = image.at<double>(i, j) +  topHatImage.at<double>(i, j) - bottomHatImage.at<double>(i, j);
+        }
+    }
+    imwrite("contrastImprovement.jpg", out);
+}
+
 int main(int argc, char** argv)
 {
-    Mat image = imread("barbara.jpg",
-                       IMREAD_COLOR);
+    //Mat image = imread("barbara.jpg",
+    //                    IMREAD_COLOR);
+
+    Mat image = imread("imagen11.jpg",
+                      IMREAD_GRAYSCALE);
+
 
     if (image.empty()) {
         cout << "Image File "
@@ -100,7 +217,11 @@ int main(int argc, char** argv)
         return -1;
     }
 
+    image.convertTo(image, CV_64F);
+
+    Mat* ptr = &image;
+    contrastEnhancementFilter(ptr);
     //medianFilter(image);
-    averageFilter(image);    
+    //averageFilter(image, 100);    
     return 0;
 }
